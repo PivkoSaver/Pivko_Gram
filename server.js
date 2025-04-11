f0faa987918f6f86bca82e21dd6522e208780f1a
PivkoSaver <pivko_saver@internet.ru>
2025-04-12 04:32:26 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,29 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
+const PORT = process.env.PORT || 3000;
+
+// Отдаём index.html
 app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
     });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
-const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
