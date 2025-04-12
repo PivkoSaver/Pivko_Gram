ea615a34a67741f79d19b99f812d4f8dfd4bb0a6
PivkoSaver <pivko_saver@internet.ru>
2025-04-12 06:56:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_sqlalchemy import SQLAlchemy
@@ -25,24 +24,11 @@ def index():
     user = User.query.get(session['user_id'])
     return render_template('index.html', nickname=user.nickname)
 
-=======
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
-
-app = Flask(__name__)
-
-# Перенаправление с главной страницы на логин
-@app.route('/')
-def index():
-    return redirect('/login')
-
->>>>>>> 808b1f77a57fa89456a3922289714c741d01c032
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         nickname = request.form['nickname']
         password = request.form['password']
-<<<<<<< HEAD
         hashed_password = generate_password_hash(password, method='sha256')
         user = User(nickname=nickname, password=hashed_password)
         db.session.add(user)
@@ -50,25 +36,11 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html')
 
-=======
-
-        conn = sqlite3.connect('users.db')
-        c = conn.cursor()
-        c.execute('CREATE TABLE IF NOT EXISTS users (nickname TEXT, password TEXT)')
-        c.execute('INSERT INTO users (nickname, password) VALUES (?, ?)', (nickname, password))
-        conn.commit()
-        conn.close()
-
-        return redirect('/login')
-    return render_template('register.html')
-
->>>>>>> 808b1f77a57fa89456a3922289714c741d01c032
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         nickname = request.form['nickname']
         password = request.form['password']
-<<<<<<< HEAD
         user = User.query.filter_by(nickname=nickname).first()
         if user and check_password_hash(user.password, password):
             session['user_id'] = user.id
@@ -102,23 +74,3 @@ with app.app_context():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port, debug=True)
-=======
-
-        conn = sqlite3.connect('users.db')
-        c = conn.cursor()
-        c.execute('SELECT * FROM users WHERE nickname = ? AND password = ?', (nickname, password))
-        user = c.fetchone()
-        conn.close()
-
-        if user:
-            return redirect('/chat')
-        else:
-            return 'Неверный логин или пароль'
-    return render_template('login.html')
-
-@app.route('/chat')
-def chat():
-    return 'Добро пожаловать в чат! (пока просто текст)'
-
-if __name__ == '__main__':
-    app.run(debug=True)
